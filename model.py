fa322846dda1dd45ce122b40a08ed5a8f391504c
Hackbright Student <info@hackbrightacademy.com>
2016-08-02 18:04:45 -0700

    Updated Rating model to have relationship to Users and Movies tables.  Started to update Flask routes and templates.

--- model.py
+++ model.py
@@ -41,6 +41,12 @@ class Movie(db.Model):
     release_at = db.Column(db.DateTime, nullable=True)
     imdb_url = db.Column(db.String(800), nullable=False)
 
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        s = "<Rating movie_id=%s title=%s release_at=%s>"
+        return s % (self.movie_id, self.title, self.release_at)
+
 
 class Rating(db.Model):
     """A movie rating to store user_id, movie_id, and score."""
@@ -48,9 +54,27 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    movie_id = db.Column(db.Integer, nullable=False)
+    # NULLABLE??!?! 
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
     score = db.Column(db.Integer, nullable=False)
+
+    # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("ratings",
+                                              order_by=rating_id))
+
+    # Define relationship to movie
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings",
+                                               order_by=rating_id))
+    
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        s = "<Rating rating_id=%s movie_id=%s user_id=%s score=%s>"
+        return s % (self.rating_id, self.movie_id, self.user_id,
+                    self.score)
 
 ##############################################################################
 # Helper functions
