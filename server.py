bc2ee14b507ddc2cee1936acffbb4b3e6d455599
Hackbright Student <info@hackbrightacademy.com>
2016-08-04 18:22:10 -0700

    Updated User Details jinja template for listing all movies user rated. Updated Movie List to show all the rated movies. Started on Movie Detail to show all the ratings for that movie and the user that provided that rating. Should start working on adding new ratings/editing existing ratings when logged in.

--- server.py
+++ server.py
@@ -36,15 +36,57 @@ def user_list():
 def show_user_info(user_id):
     """Show user information."""
 
+    user_details = User.query.filter(User.user_id == user_id).one()
+    user_age = user_details.age
+    user_zip = user_details.zipcode
+
+    complete_user_ratings = user_details.ratings
+    print complete_user_ratings
+
+    return render_template("user_detail.html", 
+                           user_id=user_id, 
+                           age=user_age, 
+                           zipcode=user_zip,
+                           ratings=complete_user_ratings)
+
+
+@app.route('/movies')
+def show_movies():
+    """Show all movies."""
+
+    movies = Movie.query.order_by('title').all()
+
+    return render_template("movies_list.html", 
+                            movies=movies)
+
+
+@app.route('/movies_list/<movie_id>')
+def movie_details(movie_id):
+    """Show movie details."""
+
+    # Getting movie object filtered on the movie_ID passed into function.
+    movie_detail = Movie.query.filter(Movie.movie_id == movie_id).one()
+    title = movie_detail.title
+    release_date = movie_detail.release_at
+    url = movie_detail.imdb_url
+
+    ratings = movie_detail.ratings
     
 
-    return render_template("user_detail.html")
+    return render_template("movie_detail.html", 
+                           movie=movie_detail, 
+                           title=title,
+                           release_date=release_date,
+                           url=url,
+                           ratings=ratings)
+
 
 @app.route('/login', methods=['GET'])
 def login():
     """User login screen."""
 
     return render_template("login.html")
+
 
 @app.route('/login', methods=['POST'])
 def login_or_create_new():
@@ -53,13 +95,15 @@ def login_or_create_new():
     email = request.form.get("user_email")
     password = request.form.get("password")
 
+    # Query the users table in database for user provided email.
     dbuser = User.query.filter(User.email == email).first()
 
     if dbuser:
         if dbuser.password == password:
             flash("Logged in")
             session["logged_in_useremail"] = email
-            return redirect("/")
+            user_url_id = str(dbuser.user_id)
+            return redirect("/users/" + user_url_id)
         else: 
             flash("Wrong Password")
             return redirect("/login")
@@ -69,16 +113,15 @@ def login_or_create_new():
         db.session.commit()
         flash("You are added.")
         session["logged_in_useremail"] = email
-        return redirect('/')
+        user_url_id = str(user.user_id)
+        return redirect('/users/' + user_url_id)
 
 
 @app.route('/logout')
 def logout():
     """User successfully loged out."""
 
-    print session.keys()
     session.pop("logged_in_useremail", None)
-    print session.keys()
     flash("You are now logged out.")
     return redirect('/')
 
